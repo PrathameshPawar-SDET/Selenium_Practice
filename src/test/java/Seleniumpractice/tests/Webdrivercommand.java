bfdc2875c89e65310af92a34f13ddefc2e5f62ef
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2025-03-02 20:23:29 +0530

    Checkbox changes

--- src/test/java/Seleniumpractice/tests/Webdrivercommand.java
+++ src/test/java/Seleniumpractice/tests/Webdrivercommand.java
@@ -1,6 +1,7 @@
 package Seleniumpractice.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import Seleniumpractice.DriverSingleton;
 import org.openqa.selenium.WebElement;
@@ -8,6 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import java.util.List;
+
 
 import java.time.Duration;
 
@@ -15,10 +18,15 @@ public class Webdrivercommand {
     WebDriver driver = DriverSingleton.getDriver();
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-    @Test
-    public void WebDriverCommands() throws InterruptedException{
+    @Test(priority = 1)
+    public void Navigations(){
         driver.get("https://demoqa.com/");
         driver.navigate().to("https://demoqa.com/elements");
+    }
+
+    @Test(priority = 2)
+    public void TextboxInteraction(){
+
         WebElement Textboxtab = driver.findElement(By.xpath("//div[@class='element-group']//div[text()='Elements']/following::li[@id='item-0'][1]"));
         wait.until(ExpectedConditions.visibilityOf(Textboxtab));
         Textboxtab.click();
@@ -34,6 +42,36 @@ public class Webdrivercommand {
         WebElement submitButton = driver.findElement(By.xpath("//button[@id='submit']"));
         submitButton.click();
 
+    }
+
+    @Test(priority = 3)
+    public void CheckboxInteraction() throws InterruptedException{
+
+        WebElement checkboxTab = driver.findElement(By.xpath("//div[@class='element-group']//div[text()='Elements']/following::span[text()='Check Box']"));
+        wait.until(ExpectedConditions.elementToBeClickable(checkboxTab));
+        checkboxTab.click();
+
+        WebElement Expandall = driver.findElement(By.xpath("//button[@class='rct-option rct-option-expand-all']"));
+        Expandall.click();
+
+
+        String[] Checboxesvalue={"Desktop", "Downloads"};
+        for(int i=0;i<Checboxesvalue.length;i++){
+            System.out.println(Checboxesvalue[i]);
+            String checkboxDynamicxpath = "//span[@class='rct-title' and text()='"+Checboxesvalue[i]+"']/preceding-sibling::span[@class='rct-checkbox']";
+            System.out.println(checkboxDynamicxpath);
+            WebElement checkboxelement = driver.findElement(By.xpath(checkboxDynamicxpath));
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true)",checkboxelement);
+            wait.until(ExpectedConditions.visibilityOf(checkboxelement));
+            checkboxelement.click();
+            Thread.sleep(3000);
+        }
+
+        List<WebElement> SelectedCheckbox = driver.findElements(By.xpath("//div[@id='result']//span[@class='text-success']"));
+        System.out.println("Following are selected checkboxes:");
+        for(WebElement sc:SelectedCheckbox){
+            System.out.println(sc.getText());
+        }
     }
 
 }

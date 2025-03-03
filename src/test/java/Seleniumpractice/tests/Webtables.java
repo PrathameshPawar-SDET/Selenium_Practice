4a0b8f131420b9afd97b2833fff01de39a309f15
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2025-03-03 23:42:54 +0530

    Webtable changes

--- src/test/java/Seleniumpractice/tests/Webtables.java
+++ src/test/java/Seleniumpractice/tests/Webtables.java
@@ -46,4 +46,41 @@ public class Webtables {
             }
         }
 
+        @Test(priority = 4)
+        public void addNewRecord(){
+
+        WebElement addbutton = driver.findElement(By.id("addNewRecordButton"));
+        addbutton.click();
+        WebElement registationModal = driver.findElement(By.id("registration-form-modal"));
+        wait.until(ExpectedConditions.visibilityOf(registationModal));
+        System.out.println("Registration Modal is Displayed...");
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        firstName.sendKeys("Prathamesh");
+
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        lastName.sendKeys("Pawar");
+
+        WebElement email = driver.findElement(By.id("userEmail"));
+        email.sendKeys("test@gmail.com");
+
+        WebElement age = driver.findElement(By.id("age"));
+        age.sendKeys("26");
+
+        WebElement salary = driver.findElement(By.id("salary"));
+        salary.sendKeys("30000");
+
+        WebElement department = driver.findElement(By.id("department"));
+        department.sendKeys("Software Testing");
+
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+
+        fetchtabledata();
+        }
+
+
+        public void editRecord(){
+
+        }
 }

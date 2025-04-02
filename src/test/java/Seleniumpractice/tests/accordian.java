54ece0438845d5ab3a3d4ff0ff822c753c5b6469
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2025-04-03 00:09:10 +0530

    Changes related to Accordian

--- src/test/java/Seleniumpractice/tests/accordian.java
+++ src/test/java/Seleniumpractice/tests/accordian.java
@@ -13,6 +13,7 @@ import org.testng.annotations.Test;
 import Seleniumpractice.DriverSingleton;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Set;
 public class accordian {
     WebDriver driver;
@@ -30,12 +31,12 @@ public class accordian {
         WebElement accordiancontent;
         WebElement activeAccordian = driver.findElement(By.id("section1Content"));
         if(activeAccordian.isDisplayed()){
-            System.out.println("If condition is running");
+//            System.out.println("If condition is running");
             accordiancontent = driver.findElement(By.cssSelector("#section1Content p"));
 
         }
         else {
-            System.out.println("Else condition is running");
+//            System.out.println("Else condition is running");
             WebElement accordianheading = driver.findElement(By.id("section1Heading"));
             ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",accordianheading);
             accordianheading.click();
@@ -50,19 +51,19 @@ public class accordian {
     @Test(priority = 2)
     public void secondAccordian(){
         WebElement accordiancontent;
-        WebElement activeAccordian = driver.findElement(By.id("section3Content"));
+        WebElement activeAccordian = driver.findElement(By.id("section2Content"));
         if(activeAccordian.isDisplayed()){
-            System.out.println("If condition is running");
-            accordiancontent = driver.findElement(By.cssSelector("#section3Content p"));
+//            System.out.println("If condition is running");
+            accordiancontent = driver.findElement(By.cssSelector("#section2Content p"));
 
         }
         else {
-            System.out.println("Else condition is running");
-            WebElement accordianheading = driver.findElement(By.id("section3Heading"));
+//            System.out.println("Else condition is running");
+            WebElement accordianheading = driver.findElement(By.id("section2Heading"));
             ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",accordianheading);
             accordianheading.click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#section3Content p")));
-            accordiancontent = driver.findElement(By.cssSelector("#section3Content p"));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#section2Content p")));
+            accordiancontent = driver.findElement(By.cssSelector("#section2Content p"));
 
         }
         System.out.println(accordiancontent.getText().substring(0,50)+"...");
@@ -72,22 +73,29 @@ public class accordian {
     @Test(priority = 3)
     public void thirdAccordian(){
         WebElement accordiancontent;
-        WebElement activeAccordian = driver.findElement(By.id("section1Content"));
+        WebElement activeAccordian = driver.findElement(By.id("section3Content"));
         if(activeAccordian.isDisplayed()){
-            System.out.println("If condition is running");
-            accordiancontent = driver.findElement(By.cssSelector("#section1Content p"));
+//            System.out.println("If condition is running");
+            accordiancontent = driver.findElement(By.cssSelector("#section3Content p"));
 
         }
         else {
-            System.out.println("Else condition is running");
-            WebElement accordianheading = driver.findElement(By.id("section1Heading"));
+//            System.out.println("Else condition is running");
+            WebElement accordianheading = driver.findElement(By.id("section3Heading"));
             ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",accordianheading);
             accordianheading.click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#section1Content p")));
-            accordiancontent = driver.findElement(By.cssSelector("#section1Content p"));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#section3Content p")));
+            accordiancontent = driver.findElement(By.cssSelector("#section3Content p"));
 
         }
         System.out.println(accordiancontent.getText().substring(0,50)+"...");
         Assert.assertTrue(accordiancontent.getText().contains("It is a long established fact that a reader will be distracted by the readable content of a page"),"Accordian 3 Content Does not matched");
+    }
+
+    @Test(priority = 4)
+    public void totalnumberofaccordians(){
+        List<WebElement> accordiancount = driver.findElements(By.xpath("//div[@class='card']"));
+        System.out.println("Total count of accordians :"+accordiancount.size());
+        Assert.assertTrue(accordiancount.size()>0,"No accordian found on this page");
     }
 }

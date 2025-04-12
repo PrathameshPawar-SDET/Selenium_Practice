77d61117e63aad461b56e6422b6055f87474fdb1
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2025-04-13 00:00:02 +0530

    [+] Draggable - testAxisRestricted

--- src/test/java/Seleniumpractice/tests/draggable.java
+++ src/test/java/Seleniumpractice/tests/draggable.java
@@ -21,11 +21,24 @@ public class draggable{
     JavascriptExecutor js;
     Actions actions;
 
+    //Simple Tab
+
     @FindBy(id = "draggableExample-tab-simple")
     WebElement simpleTab;
 
     @FindBy(css = "#draggableExample-tabpane-simple #dragBox")
     WebElement simpleDrag;
+
+    //Axis Restricted Tab
+
+    @FindBy(id = "draggableExample-tab-axisRestriction")
+    WebElement axisTab;
+
+    @FindBy(id = "restrictedX")
+    WebElement axisX;
+
+    @FindBy(id = "restrictedY")
+    WebElement axisY;
 
     @BeforeClass
     public void setup(){
@@ -36,7 +49,7 @@ public class draggable{
         driver.get("https://demoqa.com/dragabble");
     }
 
-    @Test(priority = 1)
+//    @Test(priority = 1)
     public void testSimpleDrag() throws InterruptedException{
         js.executeScript("arguments[0].scrollIntoView();",simpleTab);
         simpleTab.click();
@@ -47,5 +60,29 @@ public class draggable{
         Point finalLocation = simpleDrag.getLocation();
         System.out.println("Final location of Drag Me Button : "+finalLocation);
         Assert.assertNotEquals(initialLocation,finalLocation,"Position of Drag me button did not change");
+    }
+
+    @Test(priority = 2)
+    public void testAxisRestricted() throws InterruptedException{
+        js.executeScript("arguments[0].scrollIntoView();",axisTab);
+        axisTab.click();
+        Point initialLocationX = axisX.getLocation();
+        System.out.println("Initial location of Only-X button : "+initialLocationX);
+        actions.dragAndDropBy(axisX,100,50).perform();
+        Thread.sleep(1000);
+        Point finalLocationX = axisX.getLocation();
+        System.out.println("Final location of Only-X button : "+finalLocationX);
+        Assert.assertNotEquals(initialLocationX.getX(),finalLocationX.getX(),"X axis should be changed for Only-X");
+        Assert.assertEquals(initialLocationX.getY(),finalLocationX.getY(),"Y axis should not change of Only-X");
+
+
+        Point initialLocationy = axisY.getLocation();
+        System.out.println("Initial location of Only-Y button : "+initialLocationy);
+        actions.dragAndDropBy(axisY,100,50).perform();
+        Thread.sleep(1000);
+        Point finalLocationY = axisY.getLocation();
+        System.out.println("Final location of Only-Y button : "+finalLocationY);
+        Assert.assertNotEquals(initialLocationy.getY(),finalLocationY.getY(),"Y axis should be changed for Only-Y");
+        Assert.assertEquals(initialLocationy.getX(),finalLocationY.getX(),"X axis should not change of Only-Y");
     }
 }
